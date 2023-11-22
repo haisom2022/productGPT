88f6f45af452f47cee0f8a3f2f432ccf9ea182bf
haisom <haisom@139.com>
2023-11-22 12:07:48 +0800

    modify generate

--- app/generate/route.ts
+++ app/generate/route.ts
@@ -49,7 +49,7 @@ export async function POST(request: Request) {
       input: {
         image: imageUrl,
         prompt:
-          `a ${theme} ${room} , ${description}`,
+          `a ${theme.split(":")[0].toLowerCase()} ${room.split(":")[0].toLowerCase()} , ${description}`,
         a_prompt:
           "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning",
         n_prompt:
