5d18a4de638926a0bdf2453a6f4d23552bf0fc80
haisom <haisom@139.com>
2023-11-22 10:44:29 +0800

    modify

--- app/generate/route.ts
+++ app/generate/route.ts
@@ -49,9 +49,7 @@ export async function POST(request: Request) {
       input: {
         image: imageUrl,
         prompt:
-          room === "Gaming Room:游戏室"
-            ? "a room for gaming with gaming computers, gaming consoles, and gaming chairs"
-            : `a ${theme.split(":")[0].toLowerCase()} ${room.split(":")[0].toLowerCase()} with ${description}`,
+          `${description}`,
         a_prompt:
           "best quality, extremely detailed, photo from Pinterest, interior, cinematic photo, ultra-detailed, ultra-realistic, award-winning",
         n_prompt:
--- utils/dropdownTypes.ts
+++ utils/dropdownTypes.ts
@@ -6,15 +6,10 @@ export type themeType =
   | "Tropical:东南亚风格";
 
 export type roomType =
-  | "Living Room:客厅"
-  | "Dining Room:餐厅"
-  | "Bedroom:卧室"
-  | "Bathroom:浴室"
-  | "Office:办公室"
-  | "Gaming Room:游戏室"
-  | "Kitchen:厨房"
-  | "Playroom:活动室"
-  | "Basement:地下室";
+  | "diplay:仪表"
+  | "controller:控制器"
+  | "Dining Room:车架"
+  | "Bedroom:仪表支架";
 
 export const themes: themeType[] = [
   "Modern:现代风格",
